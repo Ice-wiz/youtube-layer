042f19a7cd28c8c47f61cc1e16286220efbc9424
ice-wiz <img_2020012@iiitm.ac.in>
2024-01-14 01:30:47 +0530

    added middleware , user schema ..in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,25 @@
 // src/server.ts
-import express from 'express';
+import express from "express";
+const mongoose = require("mongoose");
+const MONGO_URI = process.env.MONGO_URI;
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
 const port = 8000;
 
-app.get('/', (req, res) => {
-  res.send('hello from 8000');
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: "layer",
 });
+
+// app.get("/", (req, res) => {
+//   res.send("hello from 8000");
+// });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
